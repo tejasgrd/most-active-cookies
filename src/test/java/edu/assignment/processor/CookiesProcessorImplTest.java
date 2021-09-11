39dae17bc8a898622689d1fa07c593ee3b18a7a7
tejasgrd <tgarde@avaya.com>
2021-09-11 21:38:54 +0530

    adding junit and mockito dependencies

---
+++ src/test/java/edu/assignment/processor/CookiesProcessorImplTest.java
@@ -0,0 +1,10 @@
+package edu.assignment.processor;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CookiesProcessorImplTest {
+
+
+}
